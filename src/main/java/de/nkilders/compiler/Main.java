025a2c478b28e3571058adfe9b3291ff21298ec8
nkilders <71071641+nkilders@users.noreply.github.com>
2024-02-24 12:23:15 +0100

    Read code from file

--- src/main/java/de/nkilders/compiler/Main.java
+++ src/main/java/de/nkilders/compiler/Main.java
@@ -1,5 +1,9 @@
 package de.nkilders.compiler;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,13 +15,19 @@ import de.nkilders.compiler.lexer.LexerImpl;
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Lexer lexer = new LexerImpl();
-        String input = " //c\n ";
-
-        input = "+-/*()[]{}";
+        String input = loadScript("test");
 
         List<Token> tokens = lexer.tokenize(input);
         tokens.forEach(t -> LOGGER.info(t.toString()));
+    }
+
+    private static String loadScript(String filename) throws IOException {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        String pathStr = classloader.getResource("scripts/" + filename).getPath();
+        Path path = Paths.get(pathStr);
+
+        return Files.readString(path);
     }
 }
