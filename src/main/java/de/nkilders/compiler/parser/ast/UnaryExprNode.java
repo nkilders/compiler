39a5710657624a670249dc7d086b748f234e950a
nkilders <71071641+nkilders@users.noreply.github.com>
2024-04-09 18:59:46 +0200

    Add number logic to UnaryExprNode

--- src/main/java/de/nkilders/compiler/parser/ast/UnaryExprNode.java
+++ src/main/java/de/nkilders/compiler/parser/ast/UnaryExprNode.java
@@ -1,9 +1,12 @@
 package de.nkilders.compiler.parser.ast;
 
+import static de.nkilders.compiler.TokenType.MINUS;
 import static de.nkilders.compiler.TokenType.NOT;
+import static de.nkilders.compiler.TokenType.PLUS;
 
 import de.nkilders.compiler.TokenType;
 import de.nkilders.compiler.interpreter.values.BooleanValue;
+import de.nkilders.compiler.interpreter.values.NumberValue;
 import de.nkilders.compiler.interpreter.values.RuntimeValue;
 
 public class UnaryExprNode extends ExprNode {
@@ -22,6 +25,14 @@ public class UnaryExprNode extends ExprNode {
         if(expVal instanceof BooleanValue bool && operator == NOT) {
             boolean result = !bool.getValue();
             return new BooleanValue(result);
+        }
+
+        if(expVal instanceof NumberValue num && (operator == PLUS || operator == MINUS)) {
+            double result = num.getValue();
+            if(operator == MINUS) {
+                result *= -1;
+            }
+            return new NumberValue(result);
         }
 
         throw unsupportedOperation(expVal);
