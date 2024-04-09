565530cd7a5b2371efb2c1e210c0b6a6b52f6f70
nkilders <71071641+nkilders@users.noreply.github.com>
2024-04-09 19:00:33 +0200

    Suppress Warning

--- src/main/java/de/nkilders/compiler/parser/ast/AstNode.java
+++ src/main/java/de/nkilders/compiler/parser/ast/AstNode.java
@@ -6,5 +6,6 @@ public abstract class AstNode {
     /**
      * Used for node identification when printing the AST in {@link CLI}
      */
+    @SuppressWarnings("unused")
     private final String nodeType = getClass().getSimpleName();
 }
