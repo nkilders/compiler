29965bfa815539126d18e0d8bcb49ec8b261a2e1
nkilders <71071641+nkilders@users.noreply.github.com>
2024-12-29 20:37:12 +0100

    Rename col to column

--- src/main/java/de/nkilders/compiler/CompilerException.java
+++ src/main/java/de/nkilders/compiler/CompilerException.java
@@ -4,13 +4,13 @@ import de.nkilders.compiler.util.CodeLocation;
 
 public class CompilerException extends RuntimeException {
   private final int line;
-  private final int col;
+  private final int column;
 
-  public CompilerException(String message, int line, int col) {
-    super(buildMessage(message, line, col));
+  public CompilerException(String message, int line, int column) {
+    super(buildMessage(message, line, column));
 
     this.line = line;
-    this.col = col;
+    this.column = column;
   }
 
   public CompilerException(String message, CodeLocation location) {
@@ -21,11 +21,11 @@ public class CompilerException extends RuntimeException {
     return line;
   }
 
-  public int getCol() {
-    return col;
+  public int getColumn() {
+    return column;
   }
 
-  private static String buildMessage(String message, int line, int col) {
-    return String.format("%s (line %d, col %d)", message, line, col);
+  private static String buildMessage(String message, int line, int column) {
+    return String.format("%s (line %d, column %d)", message, line, column);
   }
 }
