79ec11ba98d3f840e0eed1c523d0b9140a615245
nkilders <71071641+nkilders@users.noreply.github.com>
2024-12-29 20:36:44 +0100

    Rename UtilTest to CodeLocationTest

--- src/test/java/de/nkilders/compiler/util/CodeLocationTest.java
+++ src/test/java/de/nkilders/compiler/util/CodeLocationTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-class UtilTest {
+class CodeLocationTest {
 
   @Test
-  void calculateLineAndCol() {
+  void fromTextIndex() {
     CodeLocation location = CodeLocation.fromTextIndex("\nabc", 3);
 
     assertEquals(2, location.line());
@@ -16,7 +16,7 @@ class UtilTest {
   }
 
   @Test
-  void calculateLineAndCol_posZero() {
+  void fromTextIndex_posZero() {
     CodeLocation location = CodeLocation.fromTextIndex("", 0);
 
     assertEquals(1, location.line());
@@ -24,14 +24,14 @@ class UtilTest {
   }
 
   @Test
-  void calculateLineAndCol_textNull() {
+  void fromTextIndex_textNull() {
     assertThrows(NullPointerException.class, () -> {
       CodeLocation.fromTextIndex(null, 0);
     });
   }
 
   @Test
-  void calculateLineAndCol_posNegative() {
+  void fromTextIndex_posNegative() {
     assertThrows(IndexOutOfBoundsException.class, () -> {
       CodeLocation.fromTextIndex("", -1);
     });
